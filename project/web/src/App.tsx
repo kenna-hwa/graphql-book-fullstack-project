cdf34b7266f293f668e1b7575a9f15fcbdac4b20
Kenna <kenna.hwa@gmail.com>
2023-01-24 17:15:17 +0900

    Feat: films.graphql generate update

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -3,7 +3,7 @@ import {
   ChakraProvider, theme, Box, Text
 } from "@chakra-ui/react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import FilmList from "./components/FlilmList";
+import FilmList from "./components/film/FlilmList";
 
 const apolloClient = new ApolloClient({
   uri: "http://localhost:4000/graphql",
