19de9b09a2397173fcfac27989717f23c2b72f76
Kenna <kenna.hwa@gmail.com>
2023-01-29 22:02:55 +0900

    Feat(-): 무한 스크롤
    - Waypoint 라이브러리 설치

--- project/web/src/components/film/FlilmList.tsx
+++ project/web/src/components/film/FlilmList.tsx
@@ -1,9 +1,16 @@
 import { Box, SimpleGrid, Skeleton } from '@chakra-ui/react';
+import { Waypoint } from 'react-waypoint';
 import { useFilmsQuery } from '../../generated/graphql';
 import FilmCard from './FilmCard';
 
 export default function FilmList(): JSX.Element {
-	const { data, loading, error } = useFilmsQuery();
+	const LIMIT = 6;
+	const { data, loading, error , fetchMore} = useFilmsQuery({
+		variables: {
+			limit: LIMIT,
+			cursor: 1,
+		}
+	});
 
 
 	if (error) return <p>{error.message}</p>
@@ -14,8 +21,20 @@ export default function FilmList(): JSX.Element {
 			{loading && new Array(6).fill(0).map((x) => <Skeleton key={x} height="400px"/>)}
 			{!loading && 
 				data &&
-				data.films.map((film) => (
+				data.films.films.map((film, i) => (
 						<Box key={film.id}>
+								{data.films.cursor && i === data.films.films.length - LIMIT / 2 && (
+									<Waypoint 
+										onEnter={() => {
+											fetchMore({
+												variables: {
+													limit: LIMIT,
+													cursor: data.films.cursor,
+												},
+											});
+										}}
+									/>
+								)}
 								<FilmCard film={film} />
 						</Box>
 				))}
