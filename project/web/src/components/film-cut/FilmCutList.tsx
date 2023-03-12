d33386b911550c279ca7263fd485b0a17c2805e6
Kenna <kenna.hwa@gmail.com>
2023-03-12 21:51:15 +0900

    Feat: lazyloading 구현

--- project/web/src/components/film-cut/FilmCutList.tsx
+++ project/web/src/components/film-cut/FilmCutList.tsx
@@ -1,4 +1,5 @@
 import { Box, Image, SimpleGrid, Spinner } from "@chakra-ui/react";
+import LazyLoad from "react-lazyload";
 import { useCutsQuery } from "../../generated/graphql";
 
 
@@ -20,7 +21,11 @@ function FilmCutList({ filmId }: FilmCutListProps): React.ReactElement {
 	return (
 		<SimpleGrid my={4} columns={[1, 2, null, 3]} spacing={[2, null, 8]}>
 			{data?.cuts.map((cut)=> (
+				<LazyLoad height={200} once key={cut.id}>
+					<Box>
 					<Image  src={cut.src} key={cut.id}/>
+					</Box>
+					</LazyLoad>
 				))}
 		</SimpleGrid>
 	)
