09b85e3c59ec9a1e085a518362b70eebab095374
Kenna <kenna.hwa@gmail.com>
2023-03-26 22:48:37 +0900

    Feat: 명장면 개별 모달창 구성

--- project/web/src/pages/Film.tsx
+++ project/web/src/pages/Film.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams  } from "react-router-dom";
 import CommonLayout from "../components/CommonLayout";
-import { Box, Text, Spinner  } from "@chakra-ui/react";
+import { Box, Text, Spinner, useDisclosure  } from "@chakra-ui/react";
 import { useFilmQuery } from "../generated/graphql";
 import FilmDetail from '../components/film/FilmDetail';
 import FilmCutList from "../components/film-cut/FilmCutList";
+import FilmCutModal from "../components/film-cut/FilmCutModal";
 
 
 interface FilmPageParams {
@@ -16,25 +17,33 @@ function Film(): React.ReactElement{
 	const { data, loading, error } = useFilmQuery({
 		variables: {filmId: Number(filmId)},
 	});
+	//Select cut, Modal states
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const [selectedCutId, setSelectedCutId] = useState<number>()
+	const handleCutSelect = (cutId: number) => {
+		setSelectedCutId(cutId);
+		onOpen();//ModalOpen
+	}
 
 		return (
-		<CommonLayout>
-			{ loading && <Spinner /> }
-			{ error && <Text>페이지를 표시할 수 없습니다.</Text> }
+			<CommonLayout>
+      {loading && <Spinner />}
+      {error && <Text>페이지를 표시할 수 없습니다.</Text>}
+      {filmId && data?.film ? (
+        <>
+          <FilmDetail film={data.film} />
+          <Box mt={12}>
+            <FilmCutList filmId={data.film.id} onClick={handleCutSelect} />
+          </Box>
+        </>
+      ) : (
+        <Text>페이지를 표시할 수 없습니다.</Text>
+      )}
 
-				{filmId && data?.film ? (
-					<>
-					<FilmDetail film={data.film} />
-					<Box mt={12}>
-						<FilmCutList filmId={data.film.id} />
-					</Box>
-					</>
-				) : (
-					<Text>페이지를 표시할 수 없습니다.</Text>
-				)
-			}
-
-		</CommonLayout>
+      {!selectedCutId ? null : (
+        <FilmCutModal open={isOpen} onClose={onClose} cutId={selectedCutId} />
+      )}
+    </CommonLayout>
 		);
 }
 
