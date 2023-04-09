76242b67bdb777b2b9864ca576a885f47ef63dcb
Kenna <kenna.hwa@gmail.com>
2023-04-09 22:20:29 +0900

    Fix: 회원가입을 위한 typeorm 세팅
    - DB에 user 테이블 생성
    - typeorm 버전 문제로 인해 기존 0.3.21 에서 0.2.37로 버전 다운그레이드

---
+++ project/server/src/entities/User.ts
@@ -0,0 +1,37 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import {
+	BaseEntity,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
+} from 'typeorm'
+
+@ObjectType()
+@Entity()
+export default class User extends BaseEntity {
+
+	@Field(()=>Int)
+	@PrimaryGeneratedColumn()
+	id! : number;
+
+	@Field({ description: '유저 이름' })
+	@Column({ unique: true, comment: '유저 이름' })
+	username: string;
+
+	@Field({ description: '유저 이메일' })
+	@Column({ unique: true, comment: '유저 이메일' })
+	email: string;
+
+	@Column({ comment: '비밀번호' })
+	password: string;
+
+	@Field(()=>String, { description: '생성 일자' })
+	@CreateDateColumn({ comment: '생성 일자'})
+	createAt: Date;
+
+	@Field(()=>String, { description: '업데이트 일자' })
+	@CreateDateColumn({ comment: '업데이트 일자'})
+	updateAt: Date;
+} 
