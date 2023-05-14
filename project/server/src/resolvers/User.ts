703e90af5398cfeeb14dede3511814409d1b7b2c
Kenna <kenna.hwa@gmail.com>
2023-05-14 22:02:09 +0900

    Feat: 5장 로그인 기능 구현
    - 189p 상단 server/src/resolvers/User.ts에 로그인 관련 뮤테이션 생성

--- project/server/src/resolvers/User.ts
+++ project/server/src/resolvers/User.ts
@@ -1,4 +1,4 @@
-import { Arg, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Resolver, ObjectType } from "type-graphql";
 import User from "../entities/User";
 import { IsEmail, IsString } from "class-validator";
 import  argon2  from "argon2";
@@ -8,6 +8,30 @@ export class SignUpInput {
 	@Field() @IsEmail() email: string;
 	@Field() @IsString() username: string;
 	@Field() @IsString() password: string;
+}
+
+@InputType({ description: '로그인 인풋 데이터' })
+export class LoginInput {
+	@Field() @IsString() emailOrUsername: string;
+	@Field() @IsString() password: string;
+}
+
+@ObjectType({ description: '필드 에러 타입' })
+class FieldError {
+	@Field() field: string;
+	@Field() message: string;
+}
+
+@ObjectType({ description: '로그인 반환 데이터'})
+class LoginResponse {
+	@Field(()=>[FieldError], { nullable: true })
+	errors?: FieldError[];
+
+	@Field(()=> User, { nullable: true })
+	user?: User;
+
+	@Field({ nullable: true })
+	accessToken? : string;
 }
 
 @Resolver(User)
@@ -24,5 +48,28 @@ export class UserResolver {
 		});
 		await User.insert(newUser)
 		return newUser;
+	}
+
+	@Mutation(()=>LoginResponse)
+	public async login(
+		@Arg('loginInput') loginInput : LoginInput,
+	) : Promise<LoginResponse> {
+		const { email, password } = loginInput;
+
+		const user = await User.findOne({ where: {  email  } });
+
+		if(!user)
+		return { 
+			errors: [{ field: 'email', message: '해당하는 유저가 없습니다.' }],
+		};
+
+		const isVaild = await argon2.verify(user.password, password);
+		if(!isVaild)
+		return {
+				errors: [
+					{ field: 'password', message: '비밀번호를 올바르게 입력해주세요.'}
+				],
+		};
+		return { user };
 	}
 }
