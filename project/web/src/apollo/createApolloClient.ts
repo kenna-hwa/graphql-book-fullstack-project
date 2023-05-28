aa6cb47b9e5a60a3b89b1950cc2efdb8def2e91b
Kenna <kenna.hwa@gmail.com>
2023-05-28 21:31:49 +0900

    Feat: 아폴로 클라이언트 링크 생성

--- project/web/src/apollo/createApolloClient.ts
+++ project/web/src/apollo/createApolloClient.ts
@@ -1,8 +1,46 @@
-import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+import { ApolloClient, from, HttpLink, NormalizedCacheObject, operationName } from "@apollo/client";
 import { createApolloCache } from "./createApolloCache";
+import { onError } from '@apollo/client/link/error';
+import { setContext } from '@apollo/client/link/context';
 
-export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => 
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+	if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]:-> ${operation.operationName}
+            Message: ${message}, Query: ${path}, Location: ${JSON.stringify(locations)}`,
+      );
+    });
+  }
+
+			if(networkError){
+			//eslint-disable-next-line no-console
+				console.log(`[networkError]: -> ${operation.operationName}
+				Message: ${networkError.message}`);
+			}
+	});
+
+	const httpLink = new HttpLink({
+		uri: 'http://localhost:4000/graphql',
+	});
+
+
+	const  authLink = setContext((request, prevContext) => {
+		const accessToken = localStorage.getItem('access_token');
+	
+		return {
+			headers: {
+				...prevContext.headers,
+				Authorization: accessToken ? `Bearer ${accessToken}` : '',
+			},
+		};
+	})
+	
+
+	export const createApolloClient = (): ApolloClient<NormalizedCacheObject> => 
 	new ApolloClient({
-		uri: "http://localhost:4000/graphql",
+		// uri: "http://localhost:4000/graphql",
 		cache: createApolloCache(),
+		link: from([authLink, errorLink, httpLink])
 	});
